61baedb3bdfe664ea1a1e0b6a21ea53ea89afd0e
yosrbl <yosr.bali@esprit.tn>
2021-01-18 18:26:40 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,144 @@
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+from tensorflow.keras.layers import Conv2D, Flatten, MaxPooling2D,Dense,Dropout
+from tensorflow.keras.models  import Sequential
+import numpy as np
+import tflearn
+import tensorflow
+import random
+import json
+import pickle
+
+stemmer = LancasterStemmer()
+
+with open("intents.json") as file:
+    data = json.load(file)
+try :
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(pattern)
+            docs_y.append(intent["tag"])
+
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+
+
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
+
+    labels = sorted(labels)
+
+    training = []
+    output = []
+
+    out_empty = [0 for _ in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+
+        wrds = [stemmer.stem(w) for w in doc]
+
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+
+        training.append(bag)
+        output.append(output_row)
+
+    training = np.array(training)
+    output = np.array(output)
+
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
+
+#tensorflow.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+try :
+	model.load("model.tflearn")
+except:
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
+
+def bag_of_words(s, words):
+	bag = [0 for _ in range(len(words))]
+
+	s_words = nltk.word_tokenize(s)
+	s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+	for se in s_words:
+		for i, w in enumerate(words):
+			if w == se:
+				bag[i] = 1
+	return np.array(bag)
+
+def chat():
+	print("Start talking with the bot (type quit to stop)!")
+	while True:
+		inp = input("You: ")
+		if inp.lower() == "quit":
+			break
+
+		results = model.predict([bag_of_words(inp, words)])
+        results_index = np.argmax(results)
+        tag = labels[results_index]
+
+        for tg in data["intents"]:
+            if tg['tag'] == tag:
+                responses = tg['responses']
+        print(random.choice(responses))
+
+
+chat()
+
+# tensorflow.compat.v1.reset_default_graph()
+#
+# # define the keras model
+# model = Sequential()
+# model.add(Dense(8, activation='relu', input_shape=(len(training[0]),)))
+#
+# model.add(Dense(8, activation="relu"))
+# model.add(Dense(len(output[0]), activation="softmax"))
+#
+# model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+# history = model.fit(training,output, epochs=1000, batch_size=20)
+#
+# modele=model.to_json()
+# with open("modele.json","w") as json_file:
+#     json_file.write(modele)
+# model.save_weights("modele.h5")
+
+
+
+
+
+
+
+
+
+
+
+
